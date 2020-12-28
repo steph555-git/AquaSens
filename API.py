d921b0d0a250c2ff7040bfed0828897433497c54
Bouelon <31134675+Bouelon@users.noreply.github.com>
2020-12-28 01:10:32 +0100

    commit test

--- API.py
+++ API.py
@@ -2,10 +2,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+#Page ou endpoint racine de l'API 
 @app.route("/")
 def hello():
-    return "Hello World2!"
+    return "Hello World!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
